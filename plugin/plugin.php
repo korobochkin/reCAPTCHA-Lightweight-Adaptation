5e55da8e5583f495e248b74bec3ae9edc7f57273
Kolya Korobochkin <me@korobochkin.com>
2015-07-06 18:26:37 +0300

    The main file of the plugin with WordPress header information

---
+++ plugin/plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: reCAPTCHA lightweight adaptation
+Plugin URI:  https://github.com/korobochkin/reCAPTCHA-lightweight-adaptation
+Description: A lightweight Google reCAPTCHA adaptation
+Version:     1.0.0
+Author:      Kolya Korobochkin
+Author URI:  http://korobochkin.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: recaptcha_lightweight_adaptation
+*/
